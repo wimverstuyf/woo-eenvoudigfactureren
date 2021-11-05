48bae9e01432e8ba0dad5ff021ec67d6d78c13e4
Wim Verstuyf <wim.verstuyf@eenvoudigfactureren.be>
2021-11-05 12:04:26 +0100

    update version number

--- woo-eenvoudigfactureren.php
+++ woo-eenvoudigfactureren.php
@@ -4,7 +4,7 @@
  * Description: Generate invoices of order forms in EenvoudigFactureren for WooCommerce orders.
  * Author: wimverstuyf
  * Text Domain: woo-eenvoudigfactureren
- * Version: 0.4.2
+ * Version: 0.4.3
  * Requires at least: 5.2.0
  * Requires PHP: 5.6.20
  * Domain Path: /languages
